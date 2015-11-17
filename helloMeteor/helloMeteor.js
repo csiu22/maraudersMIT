447cfa8661fffe95f877682deac2bb1e8acd2728
lindaw16 <Linda@dhcp-18-111-86-227.dyn.mit.edu>
2015-11-17 00:28:22 -0500

    cleaned up code for hello meteor

--- helloMeteor/helloMeteor.js
+++ helloMeteor/helloMeteor.js
@@ -1,19 +1,41 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  var getMap = (function() {
+        var map;
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 42.359155, lng: -71.093058},  //77 Mass Ave
+            zoom: 18
+          });
+        }
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+        </script>
+        <script async defer
+              src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmUWvkqoAwT70st_eiDUo0xcnIcUGgo9g&callback=initMap">
+        </script>
+  })();
+  
+
+
+
+
+
+// This was in the meteor tutorial. Commenting bc it might be useful
+  // // counter starts at 0
+  // Session.setDefault('counter', 0);
+
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
+
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
