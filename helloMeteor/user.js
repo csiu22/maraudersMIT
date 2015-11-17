cba7fb2687ca90ffcb4cc5243c5bb3ac75462a67
lindaw16 <Linda@dhcp-18-111-86-227.dyn.mit.edu>
2015-11-16 23:27:10 -0500

    outline for user model

---
+++ helloMeteor/user.js
@@ -0,0 +1,12 @@
+//Every user has:
+// id
+// name
+// username
+// location (latitude, longitude)
+// state - availability
+// state - status
+// TODO: character limit?
+// getLocation
+// getStatus
+// getName
+// getUsername
