dc891d56e42331436a3313c4e4b1069653d337d7
lindaw16 <Linda@dhcp-18-111-63-109.dyn.mit.edu>
2015-11-17 03:12:50 -0500

    got geolocation working! even has a little pop up saying where you are

--- MaraudersMIT/clientside.js
+++ MaraudersMIT/clientside.js
@@ -1,5 +1,20 @@
 if (Meteor.isClient) {
-    console.log("clientside code");
+	console.log("clientside code");
+
+	if ("geolocation" in navigator) {
+		/* geolocation is available */
+		console.log("geo available");
+		var map;
+		function initMap() {
+			map = new google.maps.Map(document.getElementById('map'), {
+		    		center: {lat: position.coords.latitude, lng: position.coords.longitude},  //77 Mass Ave
+		    		zoom: 18
+			});
+		}
+	} else {
+		/* geolocation IS NOT available */
+		console.log("geo not available");
+	}
 }
 
 
