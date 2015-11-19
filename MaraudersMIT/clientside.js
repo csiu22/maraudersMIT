18422929cc733f0b3d523cc1d2f6c625892dc994
Aradhana Sinha <aradhana.sinha@gmail.com>
2015-11-19 03:15:00 -0500

    jquery to get richmarker scripts

--- MaraudersMIT/clientside.js
+++ MaraudersMIT/clientside.js
@@ -1,6 +1,7 @@
 if (Meteor.isClient) {
 	console.log("clientside code");
-
+	Session.set('richmarkerReady', false);
+	Session.set('richmarkerCompiledReady', false);
 	Meteor.startup(function() {
   		Meteor.loginWithFacebook({
     			requestPermissions: ['publish_actions']
@@ -9,6 +10,18 @@ if (Meteor.isClient) {
       				Session.set('errorMessage', err.reason || 'Unknown error');
     			}
   		});
+			
+		$.getScript('https://google-maps-utility-library-v3.googlecode.com/svn-history/r435/trunk/richmarker/src/richmarker.js', function(){
+  			// script has loaded
+			console.log("richmarker available");
+  			Session.set('richmarkerReady', true);
+ 		});
+
+		$.getScript('https://google-maps-utility-library-v3.googlecode.com/svn-history/r435/trunk/richmarker/src/richmarker-compiled.js', function(){
+  			// script has loaded
+			console.log("richmarkerCompiledReady available");
+  			Session.set('richmarkerCompiledReady', true);
+ 		});
 	});
 
 	if ("geolocation" in navigator) {
