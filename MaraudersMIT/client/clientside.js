8c144dbd4c501bab6ec587256005b281fc061086
lindaw16 <Linda@dhcp-18-111-26-217.dyn.mit.edu>
2015-11-21 12:10:31 -0500

    moved the javascript logic to templates as well

--- MaraudersMIT/client/clientside.js
+++ MaraudersMIT/client/clientside.js
@@ -1,5 +1,4 @@
 if (Meteor.isClient) {
-	console.log("clientside code");
 	Meteor.startup(function() {
   		Meteor.loginWithFacebook({
     			requestPermissions: ['publish_actions']
@@ -9,21 +8,4 @@ if (Meteor.isClient) {
     			}
   		});
 	});
-
-Template.maraudersMap.rendered = function() {
-$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmUWvkqoAwT70st_eiDUo0xcnIcUGgo9g', function(){
-  			$.getScript('https://google-maps-utility-library-v3.googlecode.com/svn-history/r435/trunk/richmarker/src/richmarker-compiled.js', function(){
-  				renderMap();
- 		});
- 	});
-}
-
-Template.checkIn.rendered = function() {
-$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmUWvkqoAwT70st_eiDUo0xcnIcUGgo9g', function(){
-        $.getScript('https://google-maps-utility-library-v3.googlecode.com/svn-history/r435/trunk/richmarker/src/richmarker-compiled.js', function(){
-          renderMap();
-    });
-  });
-}
-
 }
---
+++ MaraudersMIT/client/templates/checkIn.js
@@ -0,0 +1,7 @@
+Template.checkIn.rendered = function() {
+$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmUWvkqoAwT70st_eiDUo0xcnIcUGgo9g', function(){
+        $.getScript('https://google-maps-utility-library-v3.googlecode.com/svn-history/r435/trunk/richmarker/src/richmarker-compiled.js', function(){
+          renderMap();
+    });
+  });
+}
