6ae1a8df96361a71d578bd13f7d542dbba1ed13d
Caitlin Mehl <cmehl@mit.edu>
2015-11-22 20:10:08 -0500

    saving changes before a pull

--- MaraudersMIT/client/templates/login.js
+++ MaraudersMIT/client/templates/login.js
@@ -1,4 +1,4 @@
-Template.register.events({
+Template.login.events({
 "submit .register-email": function () {      
 	// Prevent default browser form submit
 	event.preventDefault();
@@ -16,3 +16,30 @@ Template.register.events({
 	}
 }
 });
+
+
+Template.login.helpers = ({
+
+	redirect_main: function(){
+		this.route('maraudersmap', {
+  			path: '/maraudersmap'
+		});
+	},
+
+	login_route: function(){
+
+		Accounts.onLogin(function(){
+  			if(Meteor.user().isNew){
+    			Meteor.users.update(Meteor.userId(),{
+      			$set:{
+        			isNew: false
+      			}
+    		});
+    	Router.go("welcome");
+  		}
+	});
+
+	}
+
+});
+
