77370f11c5e9587e8eed3e4ed70c51a8d5594376
lindaw16 <Linda@dhcp-18-111-12-90.dyn.mit.edu>
2015-11-22 21:07:00 -0500

    yayy check in includes availibility too

--- MaraudersMIT/client/templates/checkInOverlay.js
+++ MaraudersMIT/client/templates/checkInOverlay.js
@@ -11,9 +11,12 @@ Template.checkInOverlay.events({
 	} 
 	
 	console.log ("you are " + status);
+
 	var duration = event.target.duration.value;
 	console.log("for the next " + duration + " minutes");
 
+	var availability = event.target.state.value;
+	console.log("and your availability is: " + availability);
 	Overlay.hide();
 }
 });
