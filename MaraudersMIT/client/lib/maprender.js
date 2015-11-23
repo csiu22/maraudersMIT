0e6116e9183f1498e2cdeb129625da4ff7eb9883
Connie Siu <csiu22@mit.edu>
2015-11-22 21:18:55 -0500

    Richmarker draft

--- MaraudersMIT/client/lib/maprender.js
+++ MaraudersMIT/client/lib/maprender.js
@@ -42,7 +42,7 @@ renderMap = function() {
 			var Caitlin = {
 				name: "Caitlin Mehl",
 				loc: pos,
-				pic: '/user_images/test_image.png', 
+				pic: '/user_images/test_image.png',
 				status: 'available',
 			};
 
@@ -53,7 +53,14 @@ renderMap = function() {
         status: 'busy',
       }
 			var friends = [Caitlin, Test];
-	
+
+
+// array of {name: friend's name,
+// pic: friend's pic,
+// checkin: {availability: "available" or "busy",
+// status: "hello",
+// duration: 20: loc: {lat: 10, lng: 10}}}
+
 
 		//function displayFriends(friends, map) {
 			friends.forEach(function(friend){
@@ -61,40 +68,44 @@ renderMap = function() {
           var image = {
   			    url: friend.pic,
   			    size: new google.maps.Size(40, 40),
-                  	
-  					/* These need to be set but I'm not 100% sure how to do it yet 
+
+  					/* These need to be set but I'm not 100% sure how to do it yet
   				    origin: new google.maps.Point(0, 0),
   				    anchor: new google.maps.Point(0, 32)
   				    */
           };
 
+          var location = new google.maps.LatLng(friend.loc.lat, friend.loc.lng);
+
           // Test RichMarker
-         var div = document.createElement('DIV');
-        div.innerHTML = '<div class="my-other-marker">I am flat marker!</div>';
-        marker2 = new RichMarker({
-          map: map,
-          position: {lat: function(){return 42.359155}, lng: function(){return -71.093058}}, // 77 Mass Ave
-          draggable: false,
-          flat: true,
-          anchor: RichMarkerPosition.MIDDLE,
-          content: div
-        });
-  
-  		  	var marker = new google.maps.Marker({
-                position: {lat: friend.loc.lat + .00003, lng: friend.loc.lng - .000005},
-  				      map: map,
-  				      icon: image,
-  				      //shape: shape,
-  				      title: friend.name,
-                zIndex: google.maps.Marker.MAX_ZINDEX + 1
-  		    });
-   
-          var status = new google.maps.Marker({
-              position: friend.loc,
-              map: map,
-              icon: friend.status === 'available' ? '/icons/Available.png' : '/icons/Busy.png',
-              title: friend.status,
+          // var div = document.createElement('DIV');
+          // div.innerHTML = '<div class="my-other-marker">I am flat marker!</div>';
+
+
+          marker2 = new RichMarker({
+            map: map,
+            position: location,
+            draggable: false,
+            flat: true,
+            anchor: RichMarkerPosition.MIDDLE,
+            content: '<div class=' + friend.status + '><div><div>' + friend.name + '</div><img src="' + friend.pic + '"/></div>' + '<div>' + friend.status + '</div>'
           });
+
+  		  	// var marker = new google.maps.Marker({
+       //          position: {lat: friend.loc.lat + .00003, lng: friend.loc.lng - .000005},
+  				 //      map: map,
+  				 //      icon: image,
+  				 //      //shape: shape,
+  				 //      title: friend.name,
+       //          zIndex: google.maps.Marker.MAX_ZINDEX + 1
+  		   //  });
+
+       //    var status = new google.maps.Marker({
+       //        position: friend.loc,
+       //        map: map,
+       //        icon: friend.status === 'available' ? '/icons/Available.png' : '/icons/Busy.png',
+       //        title: friend.status,
+       //    });
         }
    		});
 	}
