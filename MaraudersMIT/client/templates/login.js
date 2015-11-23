081ee40f5a89915284e01963d209e263668dea66
lindaw16 <Linda@dhcp-18-111-12-90.dyn.mit.edu>
2015-11-23 12:11:38 -0500

    rearranged the register code

--- MaraudersMIT/client/templates/login.js
+++ MaraudersMIT/client/templates/login.js
@@ -1,52 +1,17 @@
-var redirect_main = function() {
-	Router.go('maraudersMap');
-};
-
-var redirect_register = function() {
-	Router.go('newuser');
-};
-
 if (Meteor.isClient) {
   Meteor.subscribe("users");
   Accounts.onLogin(function(){
-    console.log(Meteor.user().isVerified);
+    	  console.log("Is the user verified? " + Meteor.user().isVerified);
  	  if(Meteor.user().isVerified){
-      console.log("already registered");
-	  	redirect_main();
+      		console.log("already registered");
+	  	redirect('maraudersMap');
 	  } else{
-      console.log("registering");
-	 	  redirect_register();
+      		console.log("registering");
+	 	redirect('newuser');
 	  }
   });
 
-  Template.newuser.events({
-    "submit .register-email": function () {      
-		  // Prevent default browser form submit
-      console.log("submitted");
-		  event.preventDefault();
-
-		  // Get value from form element
-
-		  var email = event.target.email.value;
-		  var domain = email.trim().toLowerCase().slice(-8);
-
-		  if (domain === "@mit.edu") {
-  		  Meteor.users.update(Meteor.userId(),{
-      		$set:{isVerified: true}
-     	  });
-    	
-        console.log("verfied!");
-    	  redirect_main();
-
-		  } else {
-			
-			  console.log("You don't even go here");
-			  alert("Please enter a valid MIT address");
-		  }
-	  },
-  });
-
-  Template.login.events({
+Template.login.events({
 	  "click #login" : function (e, tmpl) {
       console.log("logging in");
 			Meteor.loginWithFacebook(
