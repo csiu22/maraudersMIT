67f9046b4acd0214636ac57559a7944bc647233e
Tiffany Wong <tbwong@mit.edu>
2015-11-23 15:35:42 -0500

    Added method to retrieve friends' check-in data

--- MaraudersMIT/models/User-serv.js
+++ MaraudersMIT/models/User-serv.js
@@ -14,16 +14,30 @@ if (Meteor.isServer) {
         console.log("friend request sent");
       }
     },
+
     'acceptFriendRequest': function(friendId) {
       var index = Meteor.user().requests.indexOf(friendId);
       Meteor.user().requests.splice(index, 1);
       Meteor.user().friends.push(friendId);
       console.log("accepted friend request");
     },
+
     'removeFriend': function(friendId) {
       var index = Meteor.user().friends.indexOf(friendId);
       Meteor.user().friends.splice(index, 1); 
       console.log("friend removed");
+    }, 
+
+    'getFriendLocs': function(callback) {
+      var locations = [];
+      Meteor.user().friends.forEach(function(friendId) {
+        var friend = Meteor.users.findOne({_id: friendId});
+        if (friend.checkIn != null) {
+          locations.push({name: friend.services.facebook.name, pic: friend.services.facebook.pic,
+                          checkIn: friend.checkIn});
+        }
+      });
+      callback(locations);
     }
   });
 }
