15d7b0c1314a87303320efebfb3bf28206da7647
Caitlin Mehl <cmehl@mit.edu>
2015-11-25 20:47:04 -0500

    added bootstrap, had fun with sidebar icons

--- MaraudersMIT/client/templates/sideBar.js
+++ MaraudersMIT/client/templates/sideBar.js
@@ -1,5 +1,20 @@
+
+//take user to their location on the map
+var userFocus = function(){
+
+
+}
+
+
   Template.sideBar.events({
-    'click .check-in': function () {
-      Overlay.show('checkInOverlay');
+    'click #check-in': function () {
+    	userFocus();
+      	Overlay.show('checkInOverlay');
+    },
+    'click #map-icon': function() {
+    	userFocus();
     }
   });
+
+//Initialize tooltips
+Template.sideBar.rendered = function(){ $('[data-toggle="tooltip"]').tooltip();}
