6659f84e48f54e4413512e9033e1843f4f81702c
Connie Siu <csiu22@mit.edu>
2015-11-30 19:15:10 -0500

    Richmarker session for now

--- MaraudersMIT/client/lib/requirements.js
+++ MaraudersMIT/client/lib/requirements.js
@@ -1,19 +1,19 @@
-Session.set('custommarkerReady', false);
+// Session.set('custommarkerReady', false);
 
-CustomMarker = null;
-
-$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmUWvkqoAwT70st_eiDUo0xcnIcUGgo9g', function(){
-  CustomMarker = makeCustomMarker();
-  Session.set('custommarkerReady', true);
-
- });
-
-
-// Session.set('richmarkerReady', false);
-
+// CustomMarker = null;
 
 // $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmUWvkqoAwT70st_eiDUo0xcnIcUGgo9g', function(){
-//     $.getScript('https://google-maps-utility-library-v3.googlecode.com/svn-history/r435/trunk/richmarker/src/richmarker-compiled.js', function(){
-//         Session.set('richmarkerReady', true);
-//   });
+//   CustomMarker = makeCustomMarker();
+//   Session.set('custommarkerReady', true);
+
 //  });
+
+
+Session.set('richmarkerReady', false);
+
+
+$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmUWvkqoAwT70st_eiDUo0xcnIcUGgo9g', function(){
+    $.getScript('https://google-maps-utility-library-v3.googlecode.com/svn-history/r435/trunk/richmarker/src/richmarker-compiled.js', function(){
+        Session.set('richmarkerReady', true);
+  });
+ });
