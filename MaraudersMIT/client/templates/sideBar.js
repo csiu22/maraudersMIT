bbba7587a94672470bc648ade585b8c5545aee69
Tiffany Wong <tbwong@mit.edu>
2015-12-03 01:33:31 -0500

    Adding reactive sideBar and fixing tooltip bug

--- MaraudersMIT/client/templates/sideBar.js
+++ MaraudersMIT/client/templates/sideBar.js
@@ -28,23 +28,21 @@ var userFocus = function(){
 
  Template.sideBar.helpers({
     marauderFriends: function() {
-      return Template.instance().myAsyncValue.get();
+      if (Meteor.user()) {
+        var friendNames = []; 
+        if(Meteor.user() && Meteor.user().friends){
+          Meteor.user().friends.forEach(function(friendId) {
+            var friend = Meteor.users.findOne({_id: friendId});
+            friendNames.push(friend.services.facebook.name);
+          }); 
+          if (friendNames.length === 0) {
+            friendNames.push("None");
+          }   
+        }   
+        return friendNames;
+      } 
     },
   });
 
- // Sets up sideBar template so that helper functions can use async functions
-  Template.sideBar.created = function(){
-    var self = this;
-    self.myAsyncValue = new ReactiveVar([]);
-
-    Meteor.call('getMarauderFriends', function(err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        self.myAsyncValue.set(data);
-      }
-    });
-  };
-
 //Initialize tooltips
-iemplate.sideBar.rendered = function(){ $('[data-toggle="tooltip"]').tooltip();}
+template.sideBar.rendered = function(){ $('[data-toggle="tooltip"]').tooltip();}
