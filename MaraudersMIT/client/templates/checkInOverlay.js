347e526f17658545a655e58911ae88a955c02692
Caitlin Mehl <cmehl@mit.edu>
2015-12-05 14:07:55 -0500

    refreshing working instead of creating millions of clones

--- MaraudersMIT/client/templates/checkInOverlay.js
+++ MaraudersMIT/client/templates/checkInOverlay.js
@@ -29,6 +29,7 @@ Template.checkInOverlay.events({
       Meteor.call("checkIn", availability, status, duration, pos, function() {
         console.log("done submitting");
         maraudersMap.renderSelf();
+        if(!timer) {timer = setInterval(maraudersMap.refresh, 60000);}
       });
     //  checkIn(availability, status, duration, pos);
     }, function() {
