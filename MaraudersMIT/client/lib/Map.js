622c2f0e6749f0791abf9bb628cec791cbb26dab
Caitlin Mehl <cmehl@mit.edu>
2015-12-05 09:25:16 -0500

    quick code refactor

--- MaraudersMIT/client/lib/Map.js
+++ MaraudersMIT/client/lib/Map.js
@@ -9,11 +9,8 @@ Map = function(){
 
     var that = Object.create(Map.prototype);
 
-
-    var massave = new google.maps.LatLng( 42.359155,  -71.093058);
-
     var mapOptions = {
-      center: massave,
+      center: new google.maps.LatLng( 42.359155,  -71.093058), // 77 Mass Ave
       zoom: 15,
       styles: map_styles,
       mapTypeControl: false,
@@ -56,14 +53,16 @@ Map = function(){
 
     var display_status = function(user, userId, userName, userPic) {
       var status = user.checkin.availability;
-
-      // if(status != "available" && status != "busy" && status != "invisible"){
-      //       console.log("status is not valid. status = " + status);
-      //       return;
-      // }
+      var isUser = (userId === Meteor.userId() ? true : false);
+      if (isUser){
+          var fragment = "You are";
+      }
+      else{
+          var fragment = userName + " is"
+      }
 
       var marker_html = '<div id="'+ userId +'"><div class="pin">' +
-          '<div id="wrapper" class="wrapper ' + status + '">' +
+          '<div class="wrapper ' + status + '">' +
             '<div class="small">' +
               '<img src="' + userPic + '" alt="" />' +
             '</div>' +
@@ -72,14 +71,16 @@ Map = function(){
               '<p class="text"><strong>' + userName + '</strong></p>';
 
       if (status === "available") {
-        marker_html +=  '<em>You are available</em>';
+
+        marker_html +=  '<em>' + fragment +' available</em>';
       }
       else if (status === "busy") {
-        marker_html +=  '<em>You are busy</em>';
+
+         marker_html +=  '<em>' + fragment +' busy</em>';
 
       }
       else {
-        marker_html +=  '<em>You are invisible</em>';
+         marker_html +=  '<em>' + fragment +' invisible</em>';
       }
 
       marker_html += '<a class="icn close" href="#" title="Close">Close</a>' +
@@ -92,95 +93,94 @@ Map = function(){
     };
 
 
-    that.displaySelf = function(pos){
-      if(!pos || !Meteor.user()) return;
-      var gSelfLoc = new google.maps.LatLng(pos.lat, pos.lng);
-      var user_marker = new RichMarker({
-        map: that.map,
-        position: gSelfLoc,
-        draggable: false,
-        flat: true,
-        anchor: RichMarkerPosition.BOTTOM,
-        content:  display_status(Meteor.user(), Meteor.userId(), Meteor.user().profile.name, Meteor.user().profile.picture),
-
-      });
-
-      google.maps.event.addListener(user_marker, 'click', function() {
+    var addUserListeners = function(marker, userID){
+        google.maps.event.addListener(marker, 'click', function() {
         var modifier = 0.0178;
         if ($(window).width() < 768) {
           modifier = 0;
         }
 
-        if (!$('#'+Meteor.userId()).hasClass('active')) {
+        // if (!$('#'+Meteor.userId()).hasClass('active')) {
 
-        }
+        // }
 
-        $('#'+Meteor.userId()+' .pin ').removeClass('active').css('z-index', 10);
-        $('#'+Meteor.userId()+' .pin').addClass('active').css('z-index', 300);
+        $('#'+userID+' .pin ').removeClass('active').css('z-index', 10);
+        $('#'+userID+' .pin').addClass('active').css('z-index', 300);
 
-        $('#'+Meteor.userId()+' .pin #wrapper .large').click(function(){
-          $('#'+Meteor.userId()+' .pin').removeClass('active');
+        $('#'+userID+' .pin #wrapper .large').click(function(){
+          $('#'+userID+' .pin').removeClass('active');
           return false;
         });
       });
+
+
+    }
+
+
+    that.renderSelf = function(){
+        if(!Meteor.user()) return;
+        if (! "geolocation" in navigator) {
+          /* error when geolocation IS NOT available */
+          console.log("geo not available");
+          return;
+        } 
+
+        var displaySelf = function(pos){
+              that.map.setCenter(pos);
+              that.drawSelf(pos);
+          };
+
+        function drawSelf(pos){
+
+            var user_marker = new RichMarker({
+              map: that.map,
+              position: new google.maps.LatLng(pos.lat, pos.lng),
+              draggable: false,
+              flat: true,
+              anchor: RichMarkerPosition.BOTTOM,
+              content:  display_status(Meteor.user(), Meteor.userId(), Meteor.user().profile.name, Meteor.user().profile.picture),
+            });
+
+            addUserListeners(user_marker, Meteor.userId());
+
+      }
+
+       that.getUserLocation(displaySelf);
+
     }
 
     /*
     Function that displays each of the user's friends on the map
     map: a google maps object
     */
-    that.displayFriends = function(){
+    that.renderFriends = function(){
       Meteor.call("getFriendLocs", function(err, data) {
         if (err && DEBUG) {
           console.log("error occurred! " + err.toString());
         }
 
-        var friends = data;
+        if (data){
+          data.forEach(function(friend) {
+ 
+              that.renderFriend(friend);
 
-        if (friends){
-          friends.forEach(function(friend) {
-            console.log(friend);
-            // var image = {
-            //   url: friend.pic,
-            //   size: new google.maps.Size(40, 40)
-            // };
+          });
+        }
+      });
+    };
 
-            var gLoc = new google.maps.LatLng(friend.checkin.loc.lat, friend.checkin.loc.lng);
-
-            var friendMarker = new RichMarker({
+    that.renderFriend= function(friend){
+       var friendMarker = new RichMarker({
                    map: that.map,
-                   position: gLoc,
+                   position: new google.maps.LatLng(friend.checkin.loc.lat, friend.checkin.loc.lng),
                    draggable: false,
                    flat: true,
                    anchor: RichMarkerPosition.BOTTOM,
                    content: display_status(friend, friend.id, friend.name, friend.pic)
             });
 
-            google.maps.event.addListener(friendMarker, 'click', function() {
-
-              var modifier = 0.0178;
-              if ($(window).width() < 768) {
-                modifier = 0;
-              }
-
-
-              if (!$('#'+friend.id).hasClass('active')) {
-
-              }
-
-              $('#'+friend.id+' .pin ').removeClass('active').css('z-index', 10);
-              $('#'+friend.id+' .pin').addClass('active').css('z-index', 200);
-
-              $('#'+friend.id+' .pin .wrapper .large').click(function(){
-                $('#'+friend.id+' .pin').removeClass('active');
-                return false;
-              });
-            });
-          });
-        }
-      });
-    };
-
+            addUserListeners(friendMarker, friend.id);
+    }
 
     /*
     Function that returns the location of the current user, if it is available
@@ -218,21 +218,8 @@ Map = function(){
     */
 
     that.renderMap = function () {
-
-      //Geolocation is necessary in order for the user to be able to use this app
-      if ("geolocation" in navigator) {
-        var displayUsers = function(pos){
-            that.map.setCenter(pos);
-            that.displaySelf(pos);
-            that.displayFriends();
-        };
-
-        that.getUserLocation(displayUsers);
-
-      } else {
-        /* error when geolocation IS NOT available */
-        console.log("geo not available");
-      }
+        that.renderSelf;
+        that.renderFriends();
     }
 
     /*
