75db85b26a29339cf053bf0239ac490be1633ed0
Tiffany Wong <tbwong@mit.edu>
2015-12-06 23:03:38 -0500

    Removing unnecessary file

--- MaraudersMIT/server/user_data_pubs.js
+++
@@ -1,9 +0,0 @@
-// Meteor.publish("userData", function () {
-//   if (Meteor.userId()) {
-//     return Meteor.users.find({_id: Meteor.userId()},
-//                              {fields: {'email': 1,
-//                                        'text_status': 1, "availability": 1, "duration": 1}});
-//   } else {
-//     this.ready();
-//   }
-// });
